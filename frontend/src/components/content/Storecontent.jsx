1ccccf1a8291fe5bb34079354d555d4506842af9
gokul-nadaraj <167672053+gokul-nadaraj@users.noreply.github.com>
2024-10-28 12:37:25 +0530

    Update Storecontent.jsx

--- frontend/src/components/content/Storecontent.jsx
+++ frontend/src/components/content/Storecontent.jsx
@@ -10,7 +10,7 @@ const StoreContextProvider =(props)=>{
     const [cartItems,setCartItems] =useState({})
     const [token,setToken] =useState({})
 
-    const url ='http://localhost:4000'
+    const url ='https://foodorder-backend1.onrender.com'
     
 const [food_list,setFoodlist]=useState([])
 
